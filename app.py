e5524272757bc27ef5355fe90fad9f026e6bd1c0
Maha-I-Hendawy <maha.i.hendawy@gmail.com>
2025-02-19 15:52:19 +0200

    added somethings

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, request, flash, session
+from flask import Flask, request, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from ipaddress import ip_address, ip_network
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd85394dabea342b67953ddf95294'
@@ -11,6 +12,9 @@ app.config['SQLALCHEMY_DATABASE_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 app.app_context().push()
+
+
+# User Model 
 
 
 class User(db.Model):
@@ -25,17 +29,20 @@ class User(db.Model):
 
 
 
+# IPAddress Model
 
 class IPAddress(db.Model):
 	ip_id = db.Column(db.Integer, primary_key=True)
 	ip = db.Column(db.String(200), nullable=False)
+	date_created = db.Column(db.Date, default=datetime.utcnow)
+	date_updated = db.Column(db.Date)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
 	def __str__(self):
 		return f"{self.ip}"
 
 
 
-
+# IPNetwork Model
 
 class IPNetwork(db.Model):
 	net_id = db.Column(db.Integer, primary_key=True)
@@ -52,46 +59,44 @@ class IPNetwork(db.Model):
 
 # home page
 
-@app.route('/')
-def home():
-	return render_template("home.html")
 
 
 # registration page 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['POST'])
 def register():
 	if request.method == 'POST':
-		username = request.form['username']
-		email = request.form['email']
-		password = request.form['password']
-		confirm_password = request.form['confirm_password']
+		data = request.get_json()
+		username = data['username']
+		email = data['email']
+		password = data['password']
+		confirm_password = data['confirm_password']
 		if password == confirm_password:
 			hashed_password = generate_password_hash(password)
 			user = User(username=username, email=email, password=hashed_password)
 			db.session.add(user)
 			db.session.commit()
-			flash("Your account has been created")
-			return redirect(url_for('login'))
-	return render_template("register.html")
+			return jsonify({"user": user})
+	return jsonify({"method": "Please, register"})
 
 
 # login page 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
 	if request.method == 'POST':
-		username = request.form['username']
-		password = request.form['password']
+		data = request.get_json()
+		username = data['username']
+		password = data['password']
 
 		user = User.query.filter_by(username=username).first()
 		if user and check_password_hash(user.password, password):
 			session['username'] = username
 			session['user_id'] = user.user_id
-			flash("Hello " + user.username)
-			return redirect(url_for('dashboard'))
+		
+			return jsonify({"message": "Hello, " + user})
 			
-	return render_template("login.html")
+	return jsonify({"message": "Please, log in"})
 
 
 # logout route
@@ -100,7 +105,7 @@ def login():
 def logout():
 	if 'username' in session:
 		session.pop('username', default=None)
-		return redirect(url_for('home'))
+		return jsonify({"message": "You are logged out"})
 
 
 
@@ -109,11 +114,12 @@ def logout():
 
 # create ip address
 
-@app.route("/create_ip", methods=['GET', 'POST'])
-def create_ip():
+@app.route("/ips", methods=['POST'])
+def add_ip():
 	if 'username' and 'user_id' in session:
 		if request.method == 'POST':
-			ip = request.form['ip']
+			data = request.get_json()
+			ip = data['ip']
 			ipaddr = ip_address(ip)
 
 			if ipaddr:
@@ -121,78 +127,81 @@ def create_ip():
 				ipaddress = IPAddress(ip=ip_format, user_id=session['user_id'])
 				db.session.add(ipaddress)
 				db.session.commit()
-				return redirect(url_for('home'))
+				return jsonify({"ip": ipaddress})
 			else:
-				flash("invalid ip address")
-				return redirect(url_for('create_ip'))
-		return render_template("create_ip.html")
+				return jsonify("message": "invalid ip address")
+		return jsonify("message": "Add Ip Address")
 
 	else:
-		return redirect(url_for('login'))
+		return jsonify({"message": "Please, log in"})
 
 	
 
 
 # get a list of all ip address
 
-@app.route('/dashboard')
-def dashboard():
+@app.route('/ips', methods=['GET'])
+def get_ips():
 	if 'username' in session:
 
 		ips = IPAddress.query.all()
-		return render_template("dashboard.html", ips=ips)
+		return jsonify({"ips": ips})
 	else:
-		return redirect(url_for('login'))
+		return jsonify({"message": "Please, login"})
 
 
 # get ip address by id
 
-@app.route('/get_ip/<int:id>')
-def get_ip(id):
+@app.route('/ips/<int:ip_id>', methods=['GET'])
+def get_ip(ip_id):
 	if 'username' in session:
 
-		ip_address = IPAddress.query.filter_by(id=id).first()
-		return render_template("get_ip.html", ip_address=ip_address)
+		ip_address = IPAddress.query.filter_by(id=ip_id).first()
+		return jsonify({"ip_address": "ip_address" })
 
 	else:
-		return redirect(url_for('login'))
+		return jsonify({"message": "Please, log in"})
 
 # update one ip address
 
-@app.route('/update_ip/<int:id>', methods=['GET', 'POST'])
-def update_ip(id):
+@app.route('/ips/<int:ip_id>/update', methods=['PUT'])
+def update_ip(ip_id):
 	if 'username' in session:
 
 		ip_address = IPAddress.query.filter_by(id=id).first()
-		if request.method == 'POST':
-			ip = request.form['ip']
+		if request.method == 'PUT':
+			data = request.get_json()
+			ip = data['ip']
 			ip_addr = ip_address(ip)
 			if ip_addr:
 				ip_address.ip = ip_addr 
 				db.session.commit()
-				return redirect(url_for("home"))
+				return jsonify({"ip_addr": ip_addr})
 			else:
-				flash("Invalid IP Address")
+				return jsonify({"message": "invalid ip address "})
 
-		return render_template("update_ip.html")
+		return jsonify({"message": "update ip address"})
 
 	else:
-		return redirect(url_for('login'))
+		return jsonify({"message": "Please, log in"})
 
 
 # delete one ip address
 
-@app.route("/delete_ip/<int:id>")
+@app.route("/ips/<int:ip_id>/delete", methods=['DELETE'])
 def delete_ip(id):
 	if 'username' in session:
-
-		ip_address = IPAddress.query.filter_by(id=id).first()
-		db.session.delete(ip_address)
-		db.session.commit()
-		return redirect(url_for("home"))
+		if request.method == 'DELETE':
+			ip_address = IPAddress.query.filter_by(id=id).first()
+			db.session.delete(ip_address)
+			db.session.commit()
+			return jsonify({"ip_address": "ip_address"})
+		else:
+			return jsonify({"message": "Delete ip address"})
 
 	else:
-		return redirect(url_for('login'))
+		return jsonify({"message": "Please, log in"})
+
 
 
 
